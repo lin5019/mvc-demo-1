95e5acc0213327205aa4d92a6c808eb2ea89fcc9
lin <13889305019@139.com>
2020-10-26 02:06:20 +0800

    edit app1.js

--- src/app1.js
+++ src/app1.js
@@ -2,25 +2,37 @@ import $ from "jquery";
 import "./app1.css"
 
 let num;
+const $content = $(".content")
+const $add=$(".add");
+const $reduce=$(".reduce")
+const $multiply=$(".multiply")
+const $divide=$(".divide")
+num = localStorage.getItem('num')
 
-$(".add").on('click',()=>{
-    num= $(".content").text();
+$content.text(num || 200)
+
+
+$add.on('click', () => {
+    num = $content.text();
     num++
-    $(".content").text(num);
+    $content.text(num);
+    localStorage.setItem("num", num)
 })
-$(".reduce").on('click',()=>{
-    num= $(".content").text();
+$reduce.on('click', () => {
+    num = $content.text();
     num--
-    $(".content").text(num);
+    $content.text(num);
+    localStorage.setItem("num", num)
 })
-$(".multiply").on('click',()=>{
-    num= $(".content").text();
-    num*=2
-    $(".content").text(num);
+$multiply.on('click', () => {
+    num = $content.text();
+    num *= 2
+    $content.text(num);
+    localStorage.setItem("num", num)
 })
-$(".divide").on('click',()=>{
-    num= $(".content").text();
-    num/=2
-    $(".content").text(num);
+$divide.on('click', () => {
+    num = $content.text();
+    num /= 2
+    $content.text(num);
+    localStorage.setItem("num", num)
 })
-
